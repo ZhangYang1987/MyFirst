bad86188b040e2886127d3ea7517ca8a46205a6c
ZhangYang1987 <zy19870105@163.com>
2017-06-11 23:12:54 +0800

    增加决策树

--- zy20170602.py
+++ zy20170602.py
@@ -1,4 +1,4 @@
-﻿# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
 Created on Fri Jun  2 23:14:23 2017
 
@@ -8,15 +8,14 @@ Created on Fri Jun  2 23:14:23 2017
 import numpy as np
 import pandas as pd
 import scipy as sp
-
+from scipy.stats import kstest
 import sklearn as sk
+from sklearn.feature_selection import f_regression
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
 import neurolab as nl
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
-
-import sklearn as skimport neurolab as nl
-import matplotlib.pyplot as plt #记住要用pyplot这个子模块
-
 import xlrd as xlsrd
 import pygal
 import os
@@ -24,7 +23,10 @@ import math
 from pylab import mpl
 from matplotlib import gridspec 
 mpl.rcParams['font.sans-serif'] = ['SimHei']
+
+
 class Data_Wash():
+    #从xls文件读取数据
     def xlsread(self,path):
         data=xlsrd.open_workbook(path)
         table=data.sheets()[1]
@@ -47,8 +49,37 @@ class Data_Wash():
         frame_NoDupicated=frame.iloc[abc[abc==False].index]
         frame_Dupicated=frame.iloc[abc[abc==True].index]
         return frame,frame_NoDupicated,frame_Dupicated
-test=Data_Wash()
-data,data_nodup,data_dup=test.xlsread('C:/Users/zy/Documents/13#机组数据(sql).xlsx')
+
+    def preprocessing_for_tree(self,x,discretization,bins,labels):
+        B_Stats=Base_Statistics()
+        #discretization为x中需要离散化的指标
+        #计算频率分布
+        hist,bin_edges=B_Stats.histogram(discretization,bins)
+        #计算每个段的频率
+        density=list(hist/hist.sum())
+        #为每个段设置一个标识符，这里以从0开始的整数为标识
+        mark=list(range(len(bin_edges)-1))
+        #设置target_names,为标签+出现频率
+        target_names=[]
+        for i in range(len(bin_edges)-1):
+            target_names.append(labels[i]+","+str(density[i]))
+        #计算target
+        target=[]
+        for index,item in enumerate(a):
+            if item==bin_edges[bins]:
+                target.append(mark[bins-1])
+            for i in range(len(bin_edges)-1):
+                if item>=bin_edges[i] and item<bin_edges[i+1]:
+                    target.append(mark[i])
+        #创建字典
+        n_dic=dict(data=data_nodup,feature_names=["主蒸汽流量","主蒸汽温度",\
+                                          "主蒸汽压力","机组实发功率","热网抽汽流量"],\
+                                        target=target,target_names=target_names)
+        
+        return n_dic
+            
+            
+
 
 
 class Graph():
@@ -102,8 +133,7 @@ class Graph():
         for i in range(nrows):
             for j in range(ncols):
                 a.append(axes[i,j])
-#        ax0, ax1, ax2, ax3, ax4,ax5 = axes.flatten()
-#        a=list([ax0,ax1,ax2,ax3,ax4])
+
         for i in range(num_ax):
             a[i].hist([x[i]],n_bins,color=colors[i])#https://stackoverflow.com/questions/19523563/python-typeerror-int-object-is-not-iterable
             a[i].set_title(labels[i])
@@ -117,8 +147,135 @@ class Graph():
         plt.savefig("multi_hist_test.svg", format="svg")
         plt.show()
 
+class Base_Statistics():
+    
+    #频率分布计算
+    def histogram(self,x,bins):
+        hist,bin_edges=np.histogram(x,bins=bins)
+        return hist,bin_edges
+    
+    #F值和p值计算
+    def f_res(self,x,y):
+        F,p=f_regression(x,y)
+        return F,p
+    
+    #PCA计算，包含了多自变量的F值和p值计算    
+    def pca(self,x):
+        pca=PCA(n_components='mle')#注意，如果选了mle自动选取特征数，且数据超过500条即会报错'>=' not supported between instances of 'str' and 'int'
+        #pca=PCA(n_components=1)
+        pca.fit(x)
+        components=pca.components_
+        explained_variance=pca.explained_variance_
+        explained_variance_ratio=pca.explained_variance_ratio_
+        n_components=pca.n_components_
+        mean=pca.mean_
+        noise_variance=pca.noise_variance_
+        newData=pca.fit_transform(x)
+        return components,explained_variance,explained_variance_ratio,n_components,\
+                mean,noise_variance,newData
+    
+    #数据是否正态分布的检验
+    def k_s(self,x):
+        D,p=kstest(x,'norm')
+        return D,p
+    
+    #Z标准化,也叫标准差标准化
+    def Z_score(self,x):
+        scaler=preprocessing.StandardScaler().fit(x)
+        mean=scaler.mean_
+        std=scaler.std_
+        scaler_trans=scaler.transform(x)
+        return mean,std,scaler_trans
+    
+    #MinMax标准化，将数据归一到【0，1】
+    def MinMax(self,x):
+        minmax_scaler=preprocessing.MinMaxScaler().fit(x)
+        scaler_trans=minmax_scaler.transform(x)
+        return scaler_trans
+
+class Save():
+    def save_as_json(self,dic):
+        import json
+        a=json.dumps(dic)
+        return a
+    
+    def save_as_dict(self,dic):
+#        path="C:/Users/zy/python_practice/dict.txt"
+        path="./dict.txt"
+        file=open(path,'w')
+        file.write(str(dic))
+        file.close()
+
+##原始数据区
+test=Data_Wash()
+data,data_nodup,data_dup=test.xlsread('C:/Users/zy/Documents/13#机组数据(sql).xlsx')
 
 
+    
+##基础统计分析区
+B_Stats=Base_Statistics()
+#F,p=B_Stats.f_res(data_nodup.iloc[:,1:5],data_nodup[0])
+#components,explained_variance,explained_variance_ratio,n_components,\
+#                mean,noise_variance,newData=B_Stats.pca(data_nodup.iloc[0:500,1:5])
+#D,p=B_Stats.k_s(data_nodup[3])
+#mean,std,scaler_trans=B_Stats.Z_score(data_nodup)
+#mx_scaler=B_Stats.MinMax(data_nodup)
+
+##发电蒸汽单耗
+a=data_nodup[3]/(data_nodup[0]-data_nodup[4])
+
+#计算频率分布
+#hist,bin_edges=B_Stats.histogram(a,10)
+
+#计算每个段的频率
+#b=list(hist/hist.sum())
+
+#为每个段设置标签
+c=["A+","A","A-","B+","B","B-","C+","C","C-","D"]
+
+#n_dic=test.preprocessing_for_tree(data_nodup,a,10,c)
+
+save=Save()
+save.save_as_dict(n_dic)
+#a=save_json.save_as_json(n_dic)
+
+
+
+#from sklearn import tree 
+#from sklearn.tree import export_graphviz
+#from sklearn.datasets import load_iris
+##iris=load_iris()
+##
+#iris = load_iris()
+#clf = tree.DecisionTreeClassifier()
+#clf = clf.fit(data_nodup.iloc[:,0:5], data_nodup[6])
+### export the tree in Graphviz format using the export_graphviz exporter
+##with open("iris.dot", 'w') as f:
+##    f=tree.export_graphviz(clf, out_file=f)
+##    
+### predict the class of samples
+##clf.predict(iris.data[:1, :])
+### the probability of each class
+##clf.predict_proba(iris.data[:1, :])    
+##os.unlink('iris.dot')
+#os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
+#import pydotplus 
+#dot_data = tree.export_graphviz(clf, out_file=None) 
+#graph = pydotplus.graph_from_dot_data(dot_data) 
+#graph.write_pdf("zy0611.pdf") 
+#from IPython.display import Image  
+#dot_data = tree.export_graphviz(clf, out_file=None, 
+#                                feature_names=iris.feature_names,  
+#                                class_names=iris.target_names,  
+#                                filled=True, rounded=True,  
+#                                special_characters=True) 
+#graph = pydotplus.graph_from_dot_data(dot_data)  
+#Image(graph.create_png())  
+
+
+
+"""
+##作图区
 chart_title='进汽量'
 #x_label=map(str,list(data[0][1:100].index))
 y_name='进汽量'
@@ -132,3 +289,4 @@ g.multi_hist(data_nodup,10,labels,colors)
 #g.graph_line(chart_title,x_label,y_name,y_data)
 #hist,bin_edges=np.histogram(data_nodup[0],10)
 #g.graph_bar(chart_title,y_name,hist,bin_edges)
+"""
