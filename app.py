35bf5dbe8e93c09f3498a208bd68dc6d5d36bee2
ZhangYang1987 <zy19870105@163.com>
2017-10-24 17:23:12 +0800

    数据库操作
    保存数据至数据库

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from flask import Flask, jsonify,render_template,url_for,redirect,session
 from flask import request
 from flask import json
 from flask import abort
+from flask import Response
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm as Form
 
@@ -20,9 +21,9 @@ from flask_wtf import FlaskForm as Form
 #from wtforms.validators import Required 
 #from flask_wtf import TextField,StringField,SubmitField,TextAreaField,IntegerField
 from wtforms import TextField, StringField,SubmitField,TextAreaField,IntegerField,BooleanField,\
-RadioField,SelectField,SelectMultipleField
+RadioField,SelectField,SelectMultipleField,FloatField
 from wtforms import fields
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired,ValidationError
 from wtforms import validators
 
 from sklearn.externals import joblib
@@ -44,6 +45,8 @@ import random
 from pyecharts import Scatter3D
 from pyecharts.constants import DEFAULT_HOST
 from pyecharts import Bar
+from pyecharts import Pie
+from pyecharts import Boxplot
 
 app=Flask(__name__)
 #app.config.from_object('config')
@@ -79,6 +82,14 @@ y=[]
 x_linear_regression=[]
 y_linear_regression=[]
 choices=[]    
+X_train=[]
+X_test=[]
+y_train=[]
+y_test=[]
+fea_import=[]
+input_train_scaler_transform=[]
+output_train_scaler_transform=[]
+
 
 class Submit(Form):
 #    user_email = StringField("email address",[validators.Email()])
@@ -177,6 +188,19 @@ class RandomForest(Form):
     min_samples_split=StringField('节点最小分裂数')
     min_samples_leaf=StringField('叶子节点上的最小样本数')
     submit_RF=SubmitField('计算',render_kw={"style":"class:btn btn-primary"})
+
+def test_size_check(form,field):
+        if field.data>=1 or field.data<0:
+            raise ValidationError('请输入0-1之间的小数')
+            
+class Train_Test(Form):
+    test_size=FloatField('测试集比例',[DataRequired(),test_size_check],render_kw={\
+                                "placeholder":"一般选取0.2-0.4之间"})
+    random_state=StringField('随机数种子',render_kw={\
+                                "placeholder":"0"},default='0')
+    submit_Train_Test=SubmitField('提交')
+    
+    
     
 class Get_Data():
     def get_data(self,table_name):
@@ -197,6 +221,8 @@ class Get_Data():
          db.close()
          return columns_name,data_count,data
 
+#class Sclar
+
 @app.route('/login', methods=['POST','GET'])
 def login():
     error = None
@@ -206,6 +232,20 @@ def login():
         else:
             error = 'Invalid username/password'
     return render_template('login.html', error=error)
+
+@app.route('/table', methods=['POST','GET'])
+def table():
+#    return resp      
+    return render_template('table.html')
+
+@app.route('/table2', methods=['POST','GET'])
+def table2():
+    result='56fg'
+    result_json=json.dumps(result)
+    resp = Response(result_json)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp      
+#    return render_template('table.html')
 
 @app.route('/testRF', methods=['POST','GET'])
 def testRF():
@@ -319,44 +359,82 @@ def statistics():
     form1=Describe_Statistics_Submit()
     if form1.submit.data and form1.validate_on_submit():
         count,mean,std,mn,mx,per25,per50,per75,cv,kurtosis,skewness=S.descriptive_statistics(samples)
+#        Desc=pd.DataFrame([count,mean,std,mn,mx,per25,per50,per75,cv,kurtosis,skewness])
+        
+        #特别注意游标cursorclass类型，如果想以字典的形式返回，请加后边这条语句，帮助非常大，查到想打人才找到原因
+        db=pymysql.connect("localhost","zy","Zy2225786!","test1",cursorclass=pymysql.cursors.DictCursor)
+#        sql_string=str("update desc set count=%f" %count)\+str(" ,mean=%f" %mean)+str(" ,std=%f" %std)\
+#                    +str(" ,mn=%f" %mn)+str(" ,mx=%f" %mx)+str(" ,per25=%f" %per25)+str(" ,per50=%f" %per50)\
+#                    +str(" ,per75=%f" %per75)+str(" ,cv=%f" %cv)+str(" ,kurtosis=%f" %kurtosis)\
+#                    +str(" ,skewness=%f" %skewness)
+#        sql_string=str("update desc set count=%f" %count)
+        cursor=db.cursor()
+        
+#        columns_name =list(cursor.fetchall())
         for i in range(len(samples.columns)):
-            session[('columns'+str(i))]=samples.columns[i]
-            session['s1_'+str(i)]=count[i]
-            session['s2_'+str(i)]=mean[i]
-            session['s3_'+str(i)]=std[i]
-            session['s4_'+str(i)]=mn[i]
-            session['s5_'+str(i)]=mx[i]
-            session['s6_'+str(i)]=per25[i]
-            session['s7_'+str(i)]=per50[i]
-            session['s8_'+str(i)]=per75[i]
-            session['s9_'+str(i)]=cv[i]
-            session['s10_'+str(i)]=kurtosis[i]
-            session['s11_'+str(i)]=skewness[i]
+#            session[('columns'+str(i))]=samples.columns[i]
+#            session['s1_'+str(i)]=count[i]
+#            session['s2_'+str(i)]=mean[i]
+#            session['s3_'+str(i)]=std[i]
+#            session['s4_'+str(i)]=mn[i]
+#            session['s5_'+str(i)]=mx[i]
+#            session['s6_'+str(i)]=per25[i]
+#            session['s7_'+str(i)]=per50[i]
+#            session['s8_'+str(i)]=per75[i]
+#            session['s9_'+str(i)]=cv[i]
+#            session['s10_'+str(i)]=kurtosis[i]
+#            session['s11_'+str(i)]=skewness[i]
+            sql_string=str("update test1.desc set count=%f" %count[i])+str(",mean=%f" %mean[i])+str(",std=%f" %std[i])\
+                    +str(",mn=%f" %mn[i])+str(",mx=%f" %mx[i])+str(",per25=%f" %per25[i])+str(",per50=%f" %per50[i])\
+                    +str(",per75=%f" %per75[i])+str(",cv=%f" %cv[i])+str(",kurtosis=%f" %kurtosis[i])\
+                    +str(",skewness=%f" %skewness[i])+str(" where feature='%s'" %samples.columns[i])  
+                    
+#            sql_string=str("UPDATE test1.desc SET count=%f" %count[i])+str(" WHERE feature='%s'" %samples.columns[i]) 
+            cursor.execute(sql_string)
+            db.commit()
+        sql_string=str("select * from test1.desc")
+        cursor.execute(sql_string)
+        desc=cursor.fetchall()
+        session['desc']=desc   
      
+#    return render_template('statistics.html',form1=form1,form2=form2,form3=form3,form4=form4,form_pearson=form_pearson,\
+#                           columns_name0=session.get('columns_name0'),columns_name1=session.get('columns_name1'),\
+#                           columns_name2=session.get('columns_name2'),columns_name3=session.get('columns_name3'),\
+#                           columns_name4=session.get('columns_name4'),\
+#                           s1_0=session.get('s1_0'),s1_1=session.get('s1_1'),s1_2=session.get('s1_2'),s1_3=session.get('s1_3'),s1_4=session.get('s1_4'),\
+#                           s2_0=session.get('s2_0'),s2_1=session.get('s2_1'),s2_2=session.get('s2_2'),s2_3=session.get('s2_3'),s2_4=session.get('s2_4'),\
+#                           s3_0=session.get('s3_0'),s3_1=session.get('s3_1'),s3_2=session.get('s3_2'),s3_3=session.get('s3_3'),s3_4=session.get('s3_4'),\
+#                           s4_0=session.get('s4_0'),s4_1=session.get('s4_1'),s4_2=session.get('s4_2'),s4_3=session.get('s4_3'),s4_4=session.get('s4_4'),\
+#                           s5_0=session.get('s5_0'),s5_1=session.get('s5_1'),s5_2=session.get('s5_2'),s5_3=session.get('s5_3'),s5_4=session.get('s5_4'),\
+#                           s6_0=session.get('s6_0'),s6_1=session.get('s6_1'),s6_2=session.get('s6_2'),s6_3=session.get('s6_3'),s6_4=session.get('s6_4'),\
+#                           s7_0=session.get('s7_0'),s7_1=session.get('s7_1'),s7_2=session.get('s7_2'),s7_3=session.get('s7_3'),s7_4=session.get('s7_4'),\
+#                           s8_0=session.get('s8_0'),s8_1=session.get('s8_1'),s8_2=session.get('s8_2'),s8_3=session.get('s8_3'),s8_4=session.get('s8_4'),\
+#                           s9_0=session.get('s9_0'),s9_1=session.get('s9_1'),s9_2=session.get('s9_2'),s9_3=session.get('s9_3'),s9_4=session.get('s9_4'),\
+#                           s10_0=session.get('s10_0'),s10_1=session.get('s10_1'),s10_2=session.get('s10_2'),s10_3=session.get('s10_3'),s10_4=session.get('s10_4'),\
+#                           s11_0=session.get('s11_0'),s11_1=session.get('s11_1'),s11_2=session.get('s11_2'),s11_3=session.get('s11_3'),s11_4=session.get('s11_4'),\
+#                           select_item=session.get('select_item'),Selected_X=session.get('Selected_X'),Selected_Y=session.get('Selected_Y'),\
+#                           pearson=session.get('pearson'))
+    
     return render_template('statistics.html',form1=form1,form2=form2,form3=form3,form4=form4,form_pearson=form_pearson,\
-                           columns_name0=session.get('columns_name0'),columns_name1=session.get('columns_name1'),\
-                           columns_name2=session.get('columns_name2'),columns_name3=session.get('columns_name3'),\
-                           columns_name4=session.get('columns_name4'),\
-                           s1_0=session.get('s1_0'),s1_1=session.get('s1_1'),s1_2=session.get('s1_2'),s1_3=session.get('s1_3'),s1_4=session.get('s1_4'),\
-                           s2_0=session.get('s2_0'),s2_1=session.get('s2_1'),s2_2=session.get('s2_2'),s2_3=session.get('s2_3'),s2_4=session.get('s2_4'),\
-                           s3_0=session.get('s3_0'),s3_1=session.get('s3_1'),s3_2=session.get('s3_2'),s3_3=session.get('s3_3'),s3_4=session.get('s3_4'),\
-                           s4_0=session.get('s4_0'),s4_1=session.get('s4_1'),s4_2=session.get('s4_2'),s4_3=session.get('s4_3'),s4_4=session.get('s4_4'),\
-                           s5_0=session.get('s5_0'),s5_1=session.get('s5_1'),s5_2=session.get('s5_2'),s5_3=session.get('s5_3'),s5_4=session.get('s5_4'),\
-                           s6_0=session.get('s6_0'),s6_1=session.get('s6_1'),s6_2=session.get('s6_2'),s6_3=session.get('s6_3'),s6_4=session.get('s6_4'),\
-                           s7_0=session.get('s7_0'),s7_1=session.get('s7_1'),s7_2=session.get('s7_2'),s7_3=session.get('s7_3'),s7_4=session.get('s7_4'),\
-                           s8_0=session.get('s8_0'),s8_1=session.get('s8_1'),s8_2=session.get('s8_2'),s8_3=session.get('s8_3'),s8_4=session.get('s8_4'),\
-                           s9_0=session.get('s9_0'),s9_1=session.get('s9_1'),s9_2=session.get('s9_2'),s9_3=session.get('s9_3'),s9_4=session.get('s9_4'),\
-                           s10_0=session.get('s10_0'),s10_1=session.get('s10_1'),s10_2=session.get('s10_2'),s10_3=session.get('s10_3'),s10_4=session.get('s10_4'),\
-                           s11_0=session.get('s11_0'),s11_1=session.get('s11_1'),s11_2=session.get('s11_2'),s11_3=session.get('s11_3'),s11_4=session.get('s11_4'),\
+                           desc=session.get('desc'),\
                            select_item=session.get('select_item'),Selected_X=session.get('Selected_X'),Selected_Y=session.get('Selected_Y'),\
                            pearson=session.get('pearson'))
-    
+
 
 @app.route('/regression',methods=['post','get'])
 def regression():
     global choices
     global x_linear_regression
     global y_linear_regression
+    test_size=0
+    random_state=0
+    global X_train
+    global X_test
+    global y_train
+    global y_test
+    global fea_import
+    global input_train_scaler_transform
+    global output_train_scaler_transform
     form3=Selected_1()
 #    form3.item2.description='因变量ww'
     form3.item2.choices=choices    
@@ -375,22 +453,29 @@ def regression():
         session['b']=str('%.3f' %b)
         session['r']=str(r)
         session['R']=str(R)
-#        print(a)
-#        print(b)
 
     if form_multipleSelect.submit_multiple.data and form_multipleSelect.validate_on_submit():
         session['multiple_selected']=str(list(samples.columns[form_multipleSelect.item_multiple.data]))
         x_linear_regression=samples[samples.columns[form_multipleSelect.item_multiple.data]]
     
+    form_Train_Test=Train_Test()
+#    form_Train_Test.test_size.data='0.3'
+    form_Train_Test.random_state.description="如果填入整数则为固定随机数种子"
+    if form_Train_Test.submit_Train_Test.data and form_Train_Test.validate_on_submit():
+        test_size=float(form_Train_Test.test_size.data)
+        random_state=int(form_Train_Test.random_state.data)
+        X_train,X_test,y_train,y_test = train_test_split(x_linear_regression, \
+                                                            y_linear_regression, test_size=test_size, random_state=random_state)
+        session['train_sample_num']=str(len(X_train))
+        session['test_sample_num']=str(len(X_test))
     
     form_neural=NeuralNet()
+    form_neural.layers_num.data="2"
+    form_neural.neuron_num.data="[3,1]"
+    form_neural.neuron_num.description="请在[]内修改，保留[]"
     if form_neural.submit_Neural.data and form_neural.validate_on_submit():
         start_time=time.clock()
-        X_train, X_test, y_train, y_test = train_test_split(x_linear_regression, \
-                                                            y_linear_regression, test_size=0.33, random_state=42)
 
-#        Input=x_linear_regression
-#        Target=y_linear_regression.values.reshape(-1,1)
         
         Input=X_train
         Target=y_train.values.reshape(-1,1)
@@ -425,30 +510,41 @@ def regression():
     form_RF=RandomForest()
     if form_RF.submit_RF.data and form_RF.validate_on_submit():
         start_time=time.clock()
-        X_train, X_test, y_train, y_test = train_test_split(x_linear_regression, \
-                                                            y_linear_regression, test_size=0.33, random_state=42)
+      
         rf=RandomForestRegressor()
         GBDT=GradientBoostingRegressor()
         rf.fit(X_train,y_train)
+        names=X_train.columns
+        feature_importances="特征重要性排列如下：\n"+str(sorted(zip(map(lambda x: \
+                                                     str('%.5f' %x),rf.feature_importances_),names),reverse=True))
+        fea_import=rf.feature_importances_
+        
         GBDT.fit(X_train,y_train)
-#        y_predict=rf.predict(x_predict)
-        R_RF=rf.score(X_train,y_train)
-        R_GBDT=GBDT.score(X_train,y_train)
+
+        R_RF_train=rf.score(X_train,y_train)
+        R_RF_test=rf.score(X_test,y_test)
+        R_GBDT_train=GBDT.score(X_train,y_train)
+        R_GBDT_test=GBDT.score(X_test,y_test)
         end_time=time.clock()
         time_span_RF=str("%.3f" %(end_time-start_time))
-        session['R_RF']=str('%.4f' %R_RF)
+        session['R_RF_train']=str('%.4f' %R_RF_train)
         session['time_span_RF']=time_span_RF
-        session['R_GBDT']=str('%.4f' %R_GBDT)
+        session['R_GBDT_train']=str('%.4f' %R_GBDT_train)
+        session['R_RF_test']=str('%.4f' %R_RF_test)
+        session['R_GBDT_test']=str('%.4f' %R_GBDT_test)
+        session['feature_importances']=feature_importances
 
     
     return render_template('regression.html',form3=form3,form_multipleSelect=form_multipleSelect,\
-                           form_neural=form_neural,form_RF=form_RF,\
+                           form_neural=form_neural,form_RF=form_RF,form_Train_Test=form_Train_Test,\
                            multiple_selected=session.get('multiple_selected'),\
                            single_selected=session.get('single_selected'),\
+                           train_sample_num=session.get('train_sample_num'),test_sample_num=session.get('test_sample_num'),\
                            a=session.get('a'),b=session.get('b'),r=session.get('r'),R=session.get('R'),\
                            time_span=session.get('time_span'),SSE=session.get('SSE'),R_neuro=session.get('R_neuro'),\
-                           R_RF=session.get('R_RF'),time_span_RF=session.get('time_span_RF'),\
-                           R_GBDT=session.get('R_GBDT'))
+                           R_RF_train=session.get('R_RF_train'),time_span_RF=session.get('time_span_RF'),\
+                           R_GBDT_train=session.get('R_GBDT_train'),R_RF_test=session.get('R_RF_test'),\
+                           R_GBDT_test=session.get('R_GBDT_test'),feature_importances=session.get('feature_importances'))
 
 @app.route('/')
 def index():
@@ -458,37 +554,37 @@ def index():
 def feature():
     return render_template('feature.html')   
 
-@app.route('/3D')
-def hello():
-    s3d = scatter3d()
+#@app.route('/3D')
+#def hello():
+#    s3d = scatter3d()
+##    return render_template('pyecharts.html',
+##                           myechart=s3d.render_embed(),
+##                           host='C:\\Users\\zy\\Downloads\\jupyter-echarts-master\\jupyter-echarts-master\\echarts',
+##                           script_list=s3d.get_js_dependencies())
+##    return render_template('pyecharts.html',
+##                           myechart=s3d.render_embed(),
+##                           host=DEFAULT_HOST,
+##                           script_list=s3d.get_js_dependencies())
+#
 #    return render_template('pyecharts.html',
 #                           myechart=s3d.render_embed(),
-#                           host='C:\\Users\\zy\\Downloads\\jupyter-echarts-master\\jupyter-echarts-master\\echarts',
+#                           host='http://chfw.github.io/jupyter-echarts/echarts',
 #                           script_list=s3d.get_js_dependencies())
-#    return render_template('pyecharts.html',
-#                           myechart=s3d.render_embed(),
-#                           host=DEFAULT_HOST,
-#                           script_list=s3d.get_js_dependencies())
-
-    return render_template('pyecharts.html',
-                           myechart=s3d.render_embed(),
-                           host='http://chfw.github.io/jupyter-echarts/echarts',
-                           script_list=s3d.get_js_dependencies())
-
-def scatter3d():
-    data = [generate_3d_random_point() for _ in range(80)]
-    range_color = [
-        '#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf',
-        '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
-    scatter3D = Scatter3D("3D scattering plot demo", width=1200, height=600)
-    scatter3D.add("", data, is_visualmap=True, visual_range_color=range_color)
-    return scatter3D
+#
+#def scatter3d():
+#    data = [generate_3d_random_point() for _ in range(80)]
+#    range_color = [
+#        '#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf',
+#        '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
+#    scatter3D = Scatter3D("3D scattering plot demo", width=1200, height=600)
+#    scatter3D.add("", data, is_visualmap=True, visual_range_color=range_color)
+#    return scatter3D
 
 
-def generate_3d_random_point():
-    return [random.randint(0, 100),
-            random.randint(0, 100),
-            random.randint(0, 100)]
+#def generate_3d_random_point():
+#    return [random.randint(0, 100),
+#            random.randint(0, 100),
+#            random.randint(0, 100)]
 
 @app.route('/bar')
 #def sss():
@@ -515,7 +611,7 @@ def bar_render():
 
 @app.route('/hist')
 def hist():
-    h=hist_g()
+    hist_g()
     return render_template('hist.html')
 def hist_g():
     a=samples[hist_selected_column]
@@ -527,9 +623,30 @@ def hist_g():
     bar=Bar("频率分布直方图")
     bar.add(hist_selected_column,attr,result)
     bar.render('C:/Users/zy/python_practice/flask/templates/hist.html')
-    print(result)
-    print(attr)
 
+@app.route('/pie')
+def pie():
+    pie_g()
+    return render_template('pie.html')
+def pie_g():
+    attr=list(X_train.columns)
+    v1=list(fea_import)
+    pie=Pie("特征重要性")
+    pie.add("",attr,v1,is_label_show=True)
+    pie.render('C:/Users/zy/python_practice/flask/templates/pie.html')
+
+@app.route('/box')
+def box():
+    box_g()
+    return render_template('box.html')
+def box_g():
+    boxplot=Boxplot("箱形图")
+    x_axis=list(X_train.columns)
+    y_axis=input_train_scaler_transform.T
+    print(y_axis)
+    _yaxis=boxplot.prepare_data(y_axis)       # 转换数据
+    boxplot.add("boxplot", x_axis, _yaxis)
+    boxplot.render('C:/Users/zy/python_practice/flask/templates/box.html')
 
 
 @app.route('/tasks',methods=['GET'])
